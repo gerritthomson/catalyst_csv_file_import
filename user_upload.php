b5f5e8ad009b37099be5beb9aae166a10eedd465
Gerrit Thomson <gerrit.thomson@gmail.com>
2019-04-09 21:34:11 +1000

    Running but with rudimentary data error handling missing.

--- user_upload.php
+++ user_upload.php
@@ -8,16 +8,17 @@
 /*
  * variation: Read One + Store One using simple loop interator.
  */
-$fileName = 'users.csv';
+$fileName = dirname(__FILE__).DIRECTORY_SEPARATOR.'users.csv';
 
 function isEmailValid($emailAddress){
     return true;
 }
 
-$fp = fopen($fileName, 'R');
+$fp = fopen($fileName, 'r');
 while($record = fgetcsv($fp)){
+    print_r($record);
     if( ! isEmailValid($record[2])){
-        printf("Invalid email [%s] Record skipped\n");
+        printf("Invalid email [%s] Record skipped\n", $record[2]);
         continue;
     }
     // Store in db;
