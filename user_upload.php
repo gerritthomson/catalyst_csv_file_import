aaf7ffc901ab1510966fd3054c48fa6395d123bc
Gerrit Thomson <gerrit.thomson@gmail.com>
2019-04-09 21:38:09 +1000

    Check of field count in read file

--- user_upload.php
+++ user_upload.php
@@ -9,6 +9,7 @@
  * variation: Read One + Store One using simple loop interator.
  */
 $fileName = dirname(__FILE__).DIRECTORY_SEPARATOR.'users.csv';
+define('REQUIRED_FIELD_COUNT', 3);
 
 function isEmailValid($emailAddress){
     return true;
@@ -17,6 +18,10 @@ function isEmailValid($emailAddress){
 $fp = fopen($fileName, 'r');
 while($record = fgetcsv($fp)){
     print_r($record);
+    if(count($record) != REQUIRED_FIELD_COUNT){
+        printf("Invalid number of fields [%s] read, expected [%s] fields\n", count($record), REQUIRED_FIELD_COUNT);
+        continue;
+    }
     if( ! isEmailValid($record[2])){
         printf("Invalid email [%s] Record skipped\n", $record[2]);
         continue;
